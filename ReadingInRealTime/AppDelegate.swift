e4cbc7425414f37199ff2c47a01085c4574fdec3
jhchoo <jhchoo.kr@gmail.com>
2021-02-15 14:48:36 +0900

    first commit

---
+++ ReadingInRealTime/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  ReadingInRealTime
+//
+//  Created by jae hwan choo on 2021/02/15.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
