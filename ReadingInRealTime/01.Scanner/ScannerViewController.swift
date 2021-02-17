516ede4c4a96fb38c217864835b130c2ec203a95
jhchoo <jhchoo.kr@gmail.com>
2021-02-17 10:32:05 +0900

    애플 바코드 인식 테스트 완료.

---
+++ ReadingInRealTime/01.Scanner/ScannerViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ScannerViewController.swift
+//  ReadingInRealTime
+//
+//  Created by jae hwan choo on 2021/02/17.
+//
+
+import UIKit
+import AVFoundation
+
+class ScannerViewController: UIViewController {
+    
+    var captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var qrcodeFrameView: UIView?
+    
+    @IBOutlet weak var viewBox: UIView!
+    @IBOutlet weak var labelBox: UIView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var retry: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "바코드, QR";
+        
+        viewBox.layer.borderWidth = 4
+        viewBox.layer.borderColor = UIColor.yellow.cgColor
+        viewBox.layer.cornerRadius = 16
+        labelBox.layer.cornerRadius = 5
+        retry.isHidden = true
+        
+        setCapture()
+    }
+    
+    deinit {
+        print("ScannerViewController deinit")
+    }
+    
+    func setCapture() {
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+            guard let videoCaptureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) else {
+                print("ERRER 비디오 기능 불가")
+                return
+            }
+            
+            do {
+                let videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+                
+                if (self.captureSession.canAddInput(videoInput)) {
+                    self.captureSession.addInput(videoInput)
+                } else {
+                    print("ERRER 캡쳐 세션 인풋 불가")
+                    return
+                }
+                
+            } catch {
+                print("ERRER 비디오 인풋 불가")
+                return
+            }
+            
+            let metadataOutput = AVCaptureMetadataOutput()
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            
+            if (self.captureSession.canAddOutput(metadataOutput)) {
+                self.captureSession.addOutput(metadataOutput)
+                
+                // 메타 데이터 타입을 선택한것만 찾는다.
+                // 타입을 정확히 알수 없으니 모든 타입으로 정한다.
+                metadataOutput.metadataObjectTypes = metadataOutput.availableMetadataObjectTypes // [.ean8, .ean13, .pdf417, .qr]
+            } else {
+                print("ERRER 캡쳐 세션 아웃풋 불가")
+                return
+            }
+            
+            self.previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+            self.previewLayer.frame = self.view.layer.bounds
+            self.previewLayer.videoGravity = .resizeAspectFill
+            self.view.layer.addSublayer(self.previewLayer)
+            
+            // 비디오 캡쳐 시작
+            self.captureSession.startRunning()
+            
+            self.view.bringSubviewToFront(self.viewBox)
+            self.view.bringSubviewToFront(self.labelBox)
+            self.view.bringSubviewToFront(self.retry)
+            
+            self.labelName.text = "인식중"
+        }
+    }
+    
+    @IBAction func actionRetry(_ sender: Any) {
+        retry.isHidden = true
+        
+        self.labelName.text = "인식중"
+        
+        self.captureSession.startRunning()
+    }
+}
+
+extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput,
+                        didOutput metadataObjects: [AVMetadataObject],
+                        from connection: AVCaptureConnection) {
+        
+        guard let first = metadataObjects.first,
+              let readableObject = first as? AVMetadataMachineReadableCodeObject,
+              let stringValue = readableObject.stringValue else {
+            self.labelName.text = "인식중"
+            return
+        }
+        
+        // 샘플에서 찾은 바코드 타입, 종류가 많다.
+        // org.gs1.EAN-13
+        print("바코드 .type = \(readableObject.type)")
+        
+        found(code: stringValue)
+        
+        
+    }
+    
+    func found(code: String) {
+        self.labelName.text = code
+        
+        // 캡쳐 중지
+        self.captureSession.stopRunning()
+        
+        self.retry.isHidden = false
+    }
+    
+}
--- ReadingInRealTime/HomeViewController.swift
+++ ReadingInRealTime/HomeViewController.swift
@@ -9,19 +9,32 @@ import UIKit
 
 // swift text recognition
 
-//case ocrTesseract // 한글, 영문 가능
-//case ocrApple // 영문만 가능
-//case ocrGoogle // 한글, 영문 가능, 통신필요
-//case ocrKakao // 한글,영문 가능, 통신필요
+//case ocrTesseract // 오픈소스, 등록언어에 따라, 한글, 영문 가능
+//case ocrApple // 애플에서 만든  영문만 가능
+//case ocrGoogle // 이미지를 서버에 전송, 한글, 영문 가능, 통신필요 - 1000건 이상 유료
+//case ocrKakao // 이미지를 서버에 전송, 한글,영문 가능, 통신필요
 
-enum SampleType: String {
-    case barcode = "바코드 인식 Apple"
-    case businessCard = "명함인식 OCR"
-    case realTime_OCR_Apple = "실시간 글자인식 Apple" // 영문
-    case realTime_OCR_Google = "실시간 글자인식 Google" // 영문
-    case realTime_ML_Apple = "실시간 머신러닝 Apple"
-    case realTime_ML_Google = "실시간 머신러닝 Google"
-    case realTime_test = "바코드-글자인식-머신러닝"
+// 인식된 텍스트 블록에서 텍스트 추출
+enum SampleType: Int {
+    case barcode = 0
+    case businessCard
+    case realTime_OCR_Apple
+    case realTime_OCR_Google
+    case realTime_ML_Apple
+    case realTime_ML_Google
+    case realTime_test
+    
+    var vo: (Int, String) {
+        switch self {
+        case .barcode:              return (0, "바코드 인식 Apple")
+        case .businessCard:         return (1, "명함인식 OCR")
+        case .realTime_OCR_Apple:   return (2, "실시간 글자인식 Apple")
+        case .realTime_OCR_Google:  return (3, "실시간 글자인식 Google")
+        case .realTime_ML_Apple:    return (4, "실시간 머신러닝 Apple")
+        case .realTime_ML_Google:   return (5, "실시간 머신러닝 Google")
+        case .realTime_test:        return (6, "바코드-글자인식-머신러닝")
+        }
+    }
 }
 
 class HomeViewController: UIViewController {
@@ -46,30 +59,52 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        if indexPath.row == 0 {
-            cell.textLabel?.text = SampleType.barcode.rawValue
-        } else if indexPath.row == 1 {
-            cell.textLabel?.text = SampleType.businessCard.rawValue
-        } else if indexPath.row == 2 {
-            cell.textLabel?.text = SampleType.realTime_OCR_Apple.rawValue
-        } else if indexPath.row == 3 {
-            cell.textLabel?.text = SampleType.realTime_OCR_Google.rawValue
-        } else if indexPath.row == 4 {
-            cell.textLabel?.text = SampleType.realTime_ML_Apple.rawValue
-        } else if indexPath.row == 5 {
-            cell.textLabel?.text = SampleType.realTime_ML_Google.rawValue
-        } else if indexPath.row == 6 {
-            cell.textLabel?.text = SampleType.realTime_test.rawValue
-        } else {
-            cell.textLabel?.text = ""
+        switch indexPath.row {
+        case SampleType.barcode.rawValue:
+            cell.textLabel?.text = SampleType.barcode.vo.1
+        case SampleType.businessCard.rawValue:
+            cell.textLabel?.text = SampleType.businessCard.vo.1
+        case SampleType.realTime_OCR_Apple.rawValue:
+            cell.textLabel?.text = SampleType.realTime_OCR_Apple.vo.1
+        case SampleType.realTime_OCR_Google.rawValue:
+            cell.textLabel?.text = SampleType.realTime_OCR_Google.vo.1
+        case SampleType.realTime_ML_Apple.rawValue:
+            cell.textLabel?.text = SampleType.realTime_ML_Apple.vo.1
+        case SampleType.realTime_ML_Google.rawValue:
+            cell.textLabel?.text = SampleType.realTime_ML_Google.vo.1
+        case SampleType.realTime_test.rawValue:
+            cell.textLabel?.text = SampleType.realTime_test.vo.1
+        default:
+            break
         }
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-
-        
+                
+        switch indexPath.row {
+        case SampleType.barcode.rawValue:
+            let stroyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let vc = stroyboard.instantiateViewController(withIdentifier: "ScannerVC") as? ScannerViewController {
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        case SampleType.businessCard.rawValue:
+            print("11")
+        case SampleType.realTime_OCR_Apple.rawValue:
+            print("11")
+        case SampleType.realTime_OCR_Google.rawValue:
+            print("11")
+        case SampleType.realTime_ML_Apple.rawValue:
+            print("11")
+        case SampleType.realTime_ML_Google.rawValue:
+            print("11")
+        case SampleType.realTime_test.rawValue:
+            print("11")
+        default:
+            break
+        }
     }
     
 }
